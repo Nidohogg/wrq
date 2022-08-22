1f2149dd3601f0ea8550971200f406708d19bd02
Nidohogg <53866517+Nidohogg@users.noreply.github.com>
2022-08-22 15:53:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import random
+import json
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
