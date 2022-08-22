a7870d3150b9dc7c2ed6bf3c159bce7f2f2a1369
Nidohogg <53866517+Nidohogg@users.noreply.github.com>
2022-08-22 15:24:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ template_id = os.environ["TEMPLATE_ID"]
 def get_weather():
   url = "https://www.yiketianqi.com/free/day?appid=98698738&appsecret=HBaaB40E&unescape=1&city=" + city
   res = requests.get(url).json()
-  res = []
-  weather = res[0]
+  weather = list((res))
   return weather['wea'], math.floor(weather['tem']),math.floor(weather['tem_night']),math.floor(weather['tem_day'])
 
 def get_count():
